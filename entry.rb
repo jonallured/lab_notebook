f8064d1c951198633cc5ad6d8ea9e45ab3631535
Jon Allured <jon.allured@gmail.com>
2016-11-18 15:41:34 -0600

    Extract Entry to separate file

---
+++ entry.rb
@@ -0,0 +1,57 @@
+class Entry
+  def initialize(filename, markdown)
+    @filename = filename
+    @markdown = markdown
+  end
+
+  def to_html
+    "<article>#{article_html}</article>"
+  end
+
+  private
+
+  def article_html
+    [headline, tag_list, entry_html].compact.join
+  end
+
+  def headline
+    "<h1>#{@filename}</h1>"
+  end
+
+  def entry_html
+    @markdown.render entry_markdown
+  end
+
+  def entry_markdown
+    has_frontmatter? ? body : raw_content
+  end
+
+  def has_frontmatter?
+    raw_content.match /^---$/
+  end
+
+  def raw_content
+    @content ||= File.read @filename
+  end
+
+  def body
+    raw_content.split(/^---$/).last
+  end
+
+  def tag_list
+    return nil unless has_frontmatter?
+    "<p class='tags'>#{tag_links.join(' | ')}</p>"
+  end
+
+  def tag_links
+    tags.map { |tag| "<a href='#{tag}'>#{tag}</a>" }
+  end
+
+  def tags
+    frontmatter.split(': ').last.chomp.split(', ')
+  end
+
+  def frontmatter
+    raw_content.split(/^---$/).first
+  end
+end
