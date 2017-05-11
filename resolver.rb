530492d4dff05fe854dd612c367fd6809181d79a
Jon Allured <jon.allured@gmail.com>
2017-05-11 10:44:12 -0500

    WIP
    This is my first commit after coming back to this branch months later.
    What I found was that the Resolver wasn't fleshed out and there was a
    mistaken on the Dropbox path we were listening to. I also updated the
    ignore file so that this stuff isn't showing up in the diff anymore.

--- resolver.rb
+++ resolver.rb
@@ -6,44 +6,62 @@ class Resolver
   def resolve(modifications, additions, removals)
     for modified in modifications
       if modified.match /Dropbox/
-        # idk - what would this mean??
+        # this event does fire, but i don't understand what i should do with
+        # it, so i'm ignoring it for now
         puts 'dropbox modification'
         puts "  #{modified}"
       else
         puts 'local modification'
         puts "  #{modified}"
 
-
-
-        old_hash = @mapping[modified]
-        File.delete old_hash
+        stale_dropbox_path = @mapping[modified]
+        File.delete stale_dropbox_path
         tmp_path = `bin/encrypt #{modified}`.chomp
         dropbox_path = File.join Dir.home, 'Dropbox/lab_notes/entries_test', File.basename(tmp_path)
         FileUtils.cp tmp_path, dropbox_path
+        @mapping[modified] = dropbox_path
       end
     end
 
     for added in additions
       if added.match /Dropbox/
-        # use bin/decrypt to save locally
         puts 'dropbox added'
         puts "  #{added}"
+
+        local_path = `bin/decrypt #{added}`.chomp
+        @mapping[local_path] = added
       else
-        # use bin/encrypt to save to Dropbox
         puts 'local added'
         puts "  #{added}"
+
+        tmp_path = `bin/encrypt #{added}`.chomp
+        dropbox_path = File.join Dir.home, 'Dropbox/lab_notes/entries_test', File.basename(tmp_path)
+        FileUtils.cp tmp_path, dropbox_path
+        @mapping[added] = dropbox_path
       end
     end
 
     for removed in removals
       if removed.match /Dropbox/
-        # use mapping to remove locally
         puts 'dropbox removed'
         puts "  #{removed}"
+
+        local_path = @mapping.invert[removed]
+
+        if local_path
+          @mapping.delete(local_path)
+          File.delete local_path
+        end
       else
-        # use mapping to remove from Dropbox
         puts 'local removed'
         puts "  #{removed}"
+
+        dropbox_path = @mapping[removed]
+
+        if dropbox_path
+          @mapping.delete(removed)
+          File.delete dropbox_path
+        end
       end
     end
   end
