4f8b9334fc7ec7e5f7558383c3e957b1af04550e
Jon Allured <jon.allured@gmail.com>
2021-03-16 23:21:21 -0500

    Setup syntax highlighting

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "humanize"
-gem "rake"
-gem "middleman", "~> 4.2"
-gem "middleman-blog"
-gem "middleman-autoprefixer", "~> 2.7"
-gem "middleman-livereload"
-gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby, :x64_mingw]
-gem "wdm", "~> 0.1", platforms: [:mswin, :mingw, :x64_mingw]
+gem 'humanize'
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-blog'
+gem 'middleman-livereload'
+gem 'middleman-syntax'
+gem 'rake'
+gem 'redcarpet'
+gem 'tzinfo-data', platforms: %i[mswin mingw jruby x64_mingw]
+gem 'wdm', '~> 0.1', platforms: %i[mswin mingw x64_mingw]
--- config.rb
+++ config.rb
@@ -1,25 +1,30 @@
-Time.zone = "US/Central"
+Time.zone = 'US/Central'
 
-set :layout, "default"
+set :layout, 'default'
 
-page "/*.xml", layout: false
-page "/*.json", layout: false
-page "/*.txt", layout: false
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
 
 activate :blog do |blog|
-  blog.default_extension = ".md"
-  blog.layout = "default"
-  blog.new_article_template = File.expand_path("article_templates/default.erb", File.dirname(__FILE__))
-  blog.permalink = "{id}.html"
-  blog.prefix = "notes"
-  blog.sources = "general/{year}/{month}/{title}.html"
+  blog.default_extension = '.md'
+  blog.layout = 'default'
+  blog.new_article_template = File.expand_path('article_templates/default.erb', File.dirname(__FILE__))
+  blog.permalink = '{id}.html'
+  blog.prefix = 'notes'
+  blog.sources = 'general/{year}/{month}/{title}.html'
 end
 
 activate :directory_indexes
 set :trailing_slash, false
 
 activate :livereload
+
+activate :syntax, line_numbers: true
