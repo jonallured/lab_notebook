1736888e8d4bd5b28e2efda9b14a3454ef829414
Jon Allured <jon.allured@gmail.com>
2021-03-28 09:40:21 -0500

    Migrate one on one content

--- config.rb
+++ config.rb
@@ -30,3 +30,13 @@ set :trailing_slash, false
 activate :livereload
 
 activate :syntax, line_numbers: true
+
+paths = Dir.glob('source/notes/one-on-ones/*-*')
+template_path = 'notes/one-on-ones/template.html'
+ignore template_path
+paths.each do |path|
+  slug = path.split('/').last
+  locals = { slug: slug }
+  target_path = "notes/one-on-ones/#{slug}/index.html"
+  proxy target_path, template_path, locals: locals
+end
--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -8,7 +8,7 @@ class Sprint
   end
 
   def title
-    @path.split("/").last
+    @path.split('/').last
   end
 
   def url
@@ -20,9 +20,64 @@ class Sprint
   end
 end
 
+class Oneonone
+  def self.compute_for(paths)
+    paths.map { |path| new(path) }.sort
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def title
+    @path.split('/').last
+  end
+
+  def url
+    "/notes/one-on-ones/#{title}"
+  end
+
+  def <=>(other)
+    title <=> other.title
+  end
+end
+
+class Something
+  def self.compute_for(paths)
+    paths.map { |path| new(path) }.sort
+  end
+
+  def initialize(path)
+    @path = path
+  end
+
+  def title
+    @path.split('/').last.split('.').first
+  end
+
+  def url
+    slug = @path.split('/')[3]
+    "/notes/one-on-ones/#{slug}/#{title}"
+  end
+
+  def <=>(other)
+    title <=> other.title
+  end
+end
+
 module CustomHelpers
   def calculate_sprints
-    sprint_paths = Dir.glob("source/notes/sprints/20*")
+    sprint_paths = Dir.glob('source/notes/sprints/20*')
     Sprint.compute_for(sprint_paths)
+  end
+
+  def calculate_one_on_ones
+    paths = Dir.glob('source/notes/one-on-ones/*-*')
+    Oneonone.compute_for(paths)
+  end
+
+  def calculate_individual_ooos(slug)
+    paths = Dir.glob("source/notes/one-on-ones/#{slug}/*.html.md")
+    Something.compute_for(paths)
   end
 end
