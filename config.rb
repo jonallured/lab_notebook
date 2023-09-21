034fcab49089cc6507e0a52aed37b5a63df02a6d
Jon Allured <jon@jonallured.com>
2023-09-21 11:04:49 -0500

    Rearrange some things

--- config.rb
+++ config.rb
@@ -17,11 +17,10 @@ end
 
 activate :blog do |blog|
   blog.default_extension = ".md"
-  blog.layout = "general_notes"
+  blog.layout = "note"
   blog.new_article_template = File.expand_path("article_templates/default.erb", File.dirname(__FILE__))
-  blog.permalink = "{id}.html"
-  blog.prefix = "notes"
-  blog.sources = "general/{year}/{month}/{title}.html"
+  blog.permalink = "notes/{id}.html"
+  blog.sources = "notes/{year}/{month}/{title}.html"
   blog.tag_template = "tag.html"
 end
 
